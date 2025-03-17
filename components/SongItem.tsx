8409bcff2d07f161cca2c89fc2f93c9a7483a97f
Tendry24 <hei.tendry.2@gmail.com>
2025-03-17 16:14:43 +0300

    fix: set default artwork for songs without images

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,8 +1,42 @@
-import {Text, TouchableOpacity} from "react-native";
+import {Pressable, StyleSheet, Text, TouchableOpacity, useColorScheme, View} from "react-native";
 import {styles} from "@/styles/index.styles";
 import {Song} from "@/types/song";
 import useAudioStore from "@/stores/useAudioStore";
-import SongMetadata from "@/components/SongMetadata";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import {useTheme} from "@react-navigation/core";
+import {useState} from "react";
+
+function SongMetadata({song}: { song: Song }) {
+    const [tooltipVisible, setTooltipVisible] = useState(false);
+    const {colors, fonts} = useTheme();
+    const {addSongToQueue} = useAudioStore();
+    const colorScheme = useColorScheme();
+    const tooltipBgColor = colorScheme === 'dark' ? "#2a2e42" : colors.primary;
+
+    return (
+        <View style={localStyles.songItemContent}>
+            <View style={localStyles.songMetadata}>
+                <Text style={{
+                    color: colors.primary,
+                    fontFamily: fonts.heavy.fontFamily,
+                }} numberOfLines={1}>{song.title}</Text>
+                <Text style={[styles.songArtist, {
+                    color: colors.text,
+                    fontFamily: fonts.regular.fontFamily
+                }]}>{song.artist} - {song.formattedDuration}</Text>
+            </View>
+            <Pressable onPress={() => addSongToQueue(song)} style={localStyles.queueIcon}
+                       onPressIn={() => setTooltipVisible(true)} onPressOut={() => setTooltipVisible(false)}>
+                <MaterialIcons name="queue-music" color={colors.primary} size={25} style={{marginHorizontal: 'auto'}}/>
+                {tooltipVisible &&
+                    <View style={[localStyles.tooltip, {backgroundColor: tooltipBgColor}]}>
+                        <Text style={{color: colors.text, fontFamily: fonts.regular.fontFamily}}>Queued</Text>
+                    </View>
+                }
+            </Pressable>
+        </View>
+    );
+}
 
 export default function SongItem({song}: { song: Song }) {
     const {playSong} = useAudioStore();
@@ -16,3 +50,25 @@ export default function SongItem({song}: { song: Song }) {
         </TouchableOpacity>
     );
 }
+
+const localStyles = StyleSheet.create({
+    songItemContent: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "80%",
+    },
+    songMetadata: {
+        width: "80%"
+    },
+    tooltip: {
+        position: 'absolute',
+        top: 20,
+        padding: 5,
+        borderRadius: 3,
+        alignSelf: 'center',
+    },
+    queueIcon: {
+        width: '50%'
+    }
+});
